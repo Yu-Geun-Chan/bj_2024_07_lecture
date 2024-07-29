c6935669b1a1e61e7f98f7fcd67dd4ce0d6384a4
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-29 16:21:14 +0900

    ã대지 메모리초과

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,14 +1,29 @@
 package org.koreait;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        System.out.println((long)n * 4);
+        String point = "";
+        for (int i = 0; i <= n; i++) {
+            String cmd = sc.nextLine();
+            point += cmd + " ";
+        }
+        point = point.trim();
+
+        int[] pointRow = new int[point.split(" ").length / 2];
+        int[] pointColumn = new int[point.split(" ").length / 2];
+        for (int i = 0; i < point.split(" ").length / 2; i++) {
+            pointRow[i] = Integer.parseInt(point.split(" ")[i * 2]);
+            pointColumn[i] = Integer.parseInt(point.split(" ")[1 + i * 2]);
+        }
+        Arrays.sort(pointRow);
+        Arrays.sort(pointColumn);
+        System.out.println((pointRow[pointRow.length - 1] - pointRow[0]) * (pointColumn[pointColumn.length - 1] - pointColumn[0]));
 
     }
 }
