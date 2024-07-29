3d3761580696bc8f44fa8bccfacb60fe6bc4ca40
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-29 10:16:11 +0900

    기하: 직사각형과 삼각형,수학은 체육과목 입니다

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -7,24 +7,8 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String a = sc.nextLine();
-        String b = sc.nextLine();
-        String c = sc.nextLine();
-        String num = a + " " + b + " " + c;
-        String[] nums = num.split(" ");
-        int[] numsArray = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            numsArray[i] = Integer.parseInt(nums[i]);
-        }
-        if (numsArray[0] != numsArray[2] && numsArray[0] == numsArray[4]) {
-            System.out.printf(numsArray[2] + " ");
-        } else if (numsArray[0] != numsArray[4] && numsArray[0] == numsArray[2]) {
-            System.out.printf(numsArray[4] + " ");
-        } else System.out.printf(numsArray[0] + " ");
-        if (numsArray[1] != numsArray[3] && numsArray[1] == numsArray[5]) {
-            System.out.printf(numsArray[3] + " ");
-        } else if (numsArray[1] != numsArray[5] && numsArray[1] == numsArray[3]) {
-            System.out.printf(numsArray[5] + " ");
-        } else System.out.printf(numsArray[1] + " ");
+        int n = sc.nextInt();
+        System.out.println((long)n * 4);
+
     }
 }
