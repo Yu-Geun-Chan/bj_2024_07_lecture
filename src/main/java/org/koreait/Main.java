aa7a66324c71ec0b5836677ab75910649a6ff8ec
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-30 08:48:12 +0900

    기하: 직사각형과 삼각형,삼각형 외우기

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,29 +1,22 @@
 package org.koreait;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String point = "";
-        for (int i = 0; i <= n; i++) {
-            String cmd = sc.nextLine();
-            point += cmd + " ";
-        }
-        point = point.trim();
-        String[] points = point.split(" ");
-        int[] pointRow = new int[points.length / 2];
-        int[] pointColumn = new int[points.length / 2];
-        for (int i = 0; i < points.length / 2; i++) {
-            pointRow[i] = Integer.parseInt(points[i * 2]);
-            pointColumn[i] = Integer.parseInt(points[1 + i * 2]);
-        }
-        Arrays.sort(pointRow);
-        Arrays.sort(pointColumn);
-        System.out.println((pointRow[pointRow.length - 1] - pointRow[0]) * (pointColumn[pointColumn.length - 1] - pointColumn[0]));
+
+        int degree1 = sc.nextInt();
+        int degree2 = sc.nextInt();
+        int degree3 = sc.nextInt();
+        if(degree1 == 60 && degree2 == 60 && degree3 == 60){
+            System.out.print("Equilateral");
+        } else if (degree1 + degree2 + degree3 == 180 && degree1 != degree2 && degree3 != degree2 && degree1 != degree3){
+            System.out.print("Scalene");
+        } else if (degree1 + degree2 + degree3 != 180) {
+            System.out.print("Error");
+        } else System.out.print("Isosceles");
 
     }
 }
