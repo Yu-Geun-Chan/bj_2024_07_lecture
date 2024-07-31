7edbda0af5326b99bf21c739025bbac99493ae80
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-31 16:09:06 +0900

    약수, 배수와 소수,약수들의 합

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,25 +1,47 @@
 package org.koreait;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        List<Integer> numList = new ArrayList<Integer>();
-        for (int i = 1; i <= n1; i++) {
-            if (n1 % i == 0) {
-                numList.add(i);
+        while (true) {
+            String nums = br.readLine();
+
+            if (nums.equals("-1")) {
+                break;
+            }
+
+            List<Integer> numsList = new ArrayList<>();
+            for (int i = 1; i < Integer.parseInt(nums); i++) {
+                if (Integer.parseInt(nums) % i == 0) {
+                    numsList.add(i);
+                }
+            }
+            int numsSum = 0;
+
+            for (int i = 0; i < numsList.size(); i++) {
+                numsSum += numsList.get(i);
+            }
+            if (numsSum == Integer.parseInt(nums)) {
+                System.out.print(nums + " = ");
+                for (int i = 0; i < numsList.size(); i++) {
+                    System.out.print(numsList.get(i));
+                    if (i != numsList.size() - 1) {
+                        System.out.print(" + ");
+                    }
+                }
+                System.out.println();
+            } else if (numsSum != Integer.parseInt(nums)) {
+                System.out.println(Integer.parseInt(nums) + " is NOT perfect.");
             }
         }
-
-        try {
-            System.out.println(numList.get(n2 - 1));
-        } catch (Exception e) {
-            System.out.println(0);
-        }
+        br.close();
     }
 }
+
